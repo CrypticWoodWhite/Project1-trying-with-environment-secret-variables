c549b12b3f2dfd510bc887fa0cf7c2adb30fd436
Catherine Pham <v-atpham@microsoft.com>
2020-01-29 08:42:22 -0800

    js

--- .github/scripts/decrypt.sh
+++ .github/scripts/decrypt.sh
@@ -4,4 +4,4 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" \
---output $HOME/secrets/secrets.json secrets/secrets.json.gpg
+--output $HOME/secrets/secrets.js secrets/secrets.js.gpg
